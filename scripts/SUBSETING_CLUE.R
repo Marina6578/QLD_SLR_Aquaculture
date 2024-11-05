0429144ad1b368fdd40a4f7b170f514ecae94120
Marina Christofidis <74779615+Marina6578@users.noreply.github.com>
2024-11-05 13:57:02 +1000

    Delete scripts/SUBSETING_CLUE.R

--- scripts/SUBSETING_CLUE.R
+++
@@ -1,8 +0,0 @@
-
-
-library(tidyverse)
-library(here)
-
-subset_australia_DEM<-st_read(here(Practice_aquaculture/raw_data?)
-df %>%
-  filter(latitude > -50 & latitude < -6 & longitude > 110 & longitude < 155)
